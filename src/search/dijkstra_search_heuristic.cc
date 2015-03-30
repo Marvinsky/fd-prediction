667ecc4bc96b53a7d4cb3d9f3ea30d8407739a55
marvin <nivramsky@gmail.com>
2015-03-29 22:34:22 -0300

    29/03/14 dijkstra_search_heuristic.{h, cc} added

--- src/search/dijkstra_search_heuristic.cc
+++ src/search/dijkstra_search_heuristic.cc
@@ -1,9 +1,10 @@
 #include "dijkstra_search_heuristic.h"
 #include "globals.h"
-#include "operator.h"
+#include "global_operator.h"
+#include "global_state.h"
+
 #include "option_parser.h"
 #include "plugin.h"
-#include "state.h"
 
 #include "limits"
 #include <utility>
@@ -12,21 +13,21 @@
 using namespace std;
 
 DijkstraSearchHeuristic::DijkstraSearchHeuristic(const Options &opts)
-  : Heuristic(opts) {
-  min_operator_cost = 0;//numeric_limits<int>::max();
-  for (int i = 0; i < g_operators.size(); ++i) {
-    min_operator_cost = min(min_operator_cost, get_adjusted_cost(g_operators[i]));
-  }
+	: Heuristic(opts) {
+ 	min_operator_cost = 0;//numeric_limits<int>::max();
+ 	for (size_t i = 0; i < g_operators.size(); ++i) {
+ 		min_operator_cost = min(min_operator_cost, get_adjusted_cost(g_operators[i]));
+  	}
 }
 
 DijkstraSearchHeuristic::~DijkstraSearchHeuristic() {
 }
 
 void DijkstraSearchHeuristic::initialize() {
-  cout << "Initializing goal DijkstraSearchHeuristic ..." << endl;
+  cout << "Initializing DijkstraSearchHeuristic ..." << endl;
 }
 
-int DijkstraSearchHeuristic::compute_heuristic(const State &state) {
+int DijkstraSearchHeuristic::compute_heuristic(const GlobalState &state) {
   if (test_goal(state)) {
     return 0;
   } else {
--- src/search/dijkstra_search_heuristic.h
+++ src/search/dijkstra_search_heuristic.h
@@ -4,16 +4,17 @@
 #include "heuristic.h"
 
 class DijkstraSearchHeuristic : public Heuristic {
-  int min_operator_cost;
+ 
+	 int min_operator_cost;
 protected:
-  virtual void initialize();
-  virtual int compute_heuristic(const State &state);
+
+  	virtual void initialize();
+  	virtual int compute_heuristic(const GlobalState &state);
 public:
-  DijkstraSearchHeuristic(const Options &options);
-  ~DijkstraSearchHeuristic();
-  virtual void print_heur_name() {cout<<",heur:dijkstra";}
-  virtual string get_heur_name() {string temp = "dijkstra()"; return temp;}
-  virtual string get_heur_call_name() {string temp = "dijkstra()"; return temp;}
+
+  	DijkstraSearchHeuristic(const Options &options);
+  	~DijkstraSearchHeuristic();
+  	virtual string get_heur_name() {string temp = "dijkstra"; return temp;}
 };
 
 #endif
