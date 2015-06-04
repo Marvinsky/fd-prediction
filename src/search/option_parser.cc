5f3eb3e4965889ccc9aa64e0341c97ed01d27dd3
marvin <nivramsky@gmail.com>
2015-06-04 13:09:31 -0300

    04/06/15 modifying option_parser.cc in order to work with size =  2000000

--- src/search/option_parser.cc
+++ src/search/option_parser.cc
@@ -288,7 +288,7 @@ SearchEngine *OptionParser::parse_cmd_line_aux(
 		  arg2+=",size=200000,eps=60,colls=5),";
 		}*/
 		//arg2+=",size=200000,eps=30,colls=5),";
-		  arg2+=",size=20000,eps=120,colls=5),";
+		  arg2+=",size=2000000,eps=20,colls=5),";
 	      }
 	      arg2.erase(arg2.end()-1);//remove ending
 	      arg2+="]))";
